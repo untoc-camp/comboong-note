ba74a122e399f6cb65b6d67f27b22128b5fc51b8
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2024-04-02 16:09:37 +0900

    feat: 크롤링 업데이트
    - 팝업창 크롤링 업데이트(브라우저 on인 때만)
    - 모달 대신 notifications 사용 시도
    - 아이콘 이미지 추가
    - 화면 밑에 알람 뼈대만 짬

--- service-worker.js
+++ service-worker.js
@@ -58,19 +58,6 @@ async function getMajorNotices() {
   return result[0].result;
 }
 
-
-class ModalDisplayData {
-  /**
-   *
-   * @param {string} content
-   * @param {number} remainingDays
-   */
-  constructor(content, remainingDays) {
-    this.content = content;
-    this.remainingDays = remainingDays;
-  }
-}
-
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   if (reason === 'install' || reason === 'update') {
     openModal();
@@ -82,10 +69,26 @@ chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   }
 });
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.requestModalData) {
-    sendResponse({ modalDisplayData: new ModalDisplayData('종강', 60) });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.time) {
+    //팝업 설정 페이지에서 설정한 크롤링 주기를 이용하여 알람을 생성합니다.
+    chrome.alarms.create('crawlingPeriod', {
+      delayInMinutes: 1,
+      periodInMinutes: parseInt(request.time),
+    });
   }
 });
 
-
+// 나중에 크롤링한 데이터를 전후 비교하여 새 데이터만 출력되도록 짜야함
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.notifications.create(
+    {
+      type: 'basic',
+      iconUrl: 'img/iconImg.png',
+      title: 'testing',
+      message: 'doing testing..',
+      silent: false,
+    },
+    () => {},
+  );
+});
