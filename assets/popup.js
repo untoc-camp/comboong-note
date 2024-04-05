44194dff2004f2902c01d7e0a9ba0b23a6a9dce1
Squater0219 <141287114+Squater0219@users.noreply.github.com>
2024-04-05 22:06:46 +0900

    feat: 팝업창 기능 개선

--- assets/popup.js
+++ assets/popup.js
@@ -1,22 +1,30 @@
+const COLORS = ['#024d97', '#0275ce', '#02BA6E'];
+
 function renderPopup(schedules, majorNotices) {
   const scheduleList = document.getElementById('scheduleList');
   const noticeList = document.getElementById('noticeList');
 
   schedules.forEach(({ content, duration }) => {
-    const li = document.createElement('li');
-    li.textContent = `${content} - ${duration}`;
-    scheduleList.appendChild(li);
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    td1.textContent = content;
+    td2.textContent = duration.replace(/2024-/g, '');
+    scheduleList.append(tr);
+    tr.append(td1, td2);
   });
 
   majorNotices.forEach(({ articleTitle, articleHref, articleWriter }) => {
-    const li = document.createElement('li');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
     const link = document.createElement('a');
     link.href = articleHref;
     link.target = '_blank';
     link.textContent = articleTitle;
-    li.appendChild(link);
-    li.appendChild(document.createTextNode(` (${articleWriter})`));
-    noticeList.appendChild(li);
+    td.appendChild(link);
+    td.appendChild(document.createTextNode(` (${articleWriter})`));
+    noticeList.append(tr);
+    tr.append(td);
   });
 }
 
@@ -24,6 +32,19 @@ async function fetchAndRender() {
   const { schedules = [], majorNotices = [] } = await chrome.storage.local.get(['schedules', 'majorNotices']);
   renderPopup(schedules, majorNotices);
 }
+
+const tabs = document.querySelectorAll('.tab-menu li');
+const contents = document.querySelectorAll('.tab-content .content');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    contents.forEach((content) => content.classList.remove('current'));
+    contents[index].classList.add('current');
+
+    const content_margin = document.getElementById('popup-edge');
+    content_margin.style.backgroundColor = COLORS[index];
+  });
+});
 
 fetchAndRender();
 
