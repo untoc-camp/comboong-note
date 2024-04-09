44c993219fc68aff874f0445cb254f50211b38f2
Squater0219 <141287114+Squater0219@users.noreply.github.com>
2024-04-09 17:00:57 +0900

    feat: 설정 창 추가 및 코드수정

--- assets/popup.js
+++ assets/popup.js
@@ -1,8 +1,7 @@
-const COLORS = ['#024d97', '#0275ce', '#02BA6E'];
-
 function renderPopup(schedules, majorNotices) {
   const scheduleList = document.getElementById('scheduleList');
   const noticeList = document.getElementById('noticeList');
+  const generalNotices = [];
 
   schedules.forEach(({ content, duration }) => {
     const tr = document.createElement('tr');
@@ -12,6 +11,12 @@ function renderPopup(schedules, majorNotices) {
     td2.textContent = duration.replace(/2024-/g, '');
     scheduleList.append(tr);
     tr.append(td1, td2);
+  });
+
+  majorNotices.forEach((notice) => {
+    if (notice.articleTitle.startsWith('[ 일반공지 ]')) {
+      generalNotices.push(notice);
+    }
   });
 
   majorNotices.forEach(({ articleTitle, articleHref, articleWriter }) => {
@@ -25,6 +30,12 @@ function renderPopup(schedules, majorNotices) {
     td.appendChild(document.createTextNode(` (${articleWriter})`));
     noticeList.append(tr);
     tr.append(td);
+
+    generalNotices.forEach((notice) => {
+      if (notice.articleTitle == articleTitle) {
+        tr.classList.add('generalNotice', 'hide');
+      }
+    });
   });
 }
 
@@ -33,20 +44,71 @@ async function fetchAndRender() {
   renderPopup(schedules, majorNotices);
 }
 
-const tabs = document.querySelectorAll('.tab-menu li');
-const contents = document.querySelectorAll('.tab-content .content');
+function tabEventListener() {
+  const tabs = document.querySelectorAll('.tab-menu li');
+  const contents = document.querySelectorAll('.tab-content .content');
+  const content_margin = document.getElementById('popup-edge');
 
-tabs.forEach((tab, index) => {
-  tab.addEventListener('click', () => {
-    contents.forEach((content) => content.classList.remove('current'));
-    contents[index].classList.add('current');
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      contents.forEach((content) => content.classList.remove('current'));
+      contents[index].classList.add('current');
 
-    const content_margin = document.getElementById('popup-edge');
-    content_margin.style.backgroundColor = COLORS[index];
+      content_margin.classList.remove('color-1', 'color-2', 'color-3');
+      content_margin.classList.add(`color-${index + 1}`);
+    });
   });
-});
+}
+
+function majorNoticesToggle() {
+  const toggle = document.querySelector('#majorNoticeToggle');
+  const text = document.getElementById('toggleText');
+
+  toggle.addEventListener('click', () => {
+    const generalNotices = document.querySelectorAll('.generalNotice');
+    generalNotices.forEach((tr) => {
+      tr.classList.toggle('hide');
+    });
+    if(text.innerText=="일반공지 닫기"){
+      text.innerText="일반공지 열기";
+    }else{
+      text.innerText="일반공지 닫기";
+    }
+  });
+}
+
+function dropDownList() {
+  const dropdownBtns = document.querySelectorAll('.dropbtn');
+  const listItems = document.querySelectorAll('.list');
+
+  dropdownBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const dropdownContent = btn.nextElementSibling;
+      dropdownContent.style.display = dropdownContent.style.display === 'none' ? 'block' : 'none';
+    });
+  });
+
+  listItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      const dropbtnContent = item.closest('.dropdown').querySelector('.dropbtn_content');
+      dropbtnContent.textContent = item.textContent;
+    });
+  });
+
+  window.addEventListener('click', (e) => {
+    if (!e.target.matches('.dropbtn')) {
+      const dropdownContents = document.querySelectorAll('.dropdown-content');
+      dropdownContents.forEach((content) => {
+        content.style.display = 'none';
+      });
+    }
+  });
+}
 
 fetchAndRender();
+tabEventListener();
+dropDownList();
+majorNoticesToggle();
 
 (async () => {
   // 차후 설정한 주기 시간으로 변경
