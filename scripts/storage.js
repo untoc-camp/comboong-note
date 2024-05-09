526d0633aa336a3d3f6ccc9195a58d30511e6cf4
Squater0219 <141287114+Squater0219@users.noreply.github.com>
2024-05-09 17:04:29 +0900

    refactor: 스토리지 리팩터링

--- scripts/storage.js
+++ scripts/storage.js
@@ -2,7 +2,10 @@ const localStorageSet = (content) => {
   chrome.storage.local.set(content);
 };
 
-const localStorageGet = (content) => {};
-// popup.js의 fetchAndRender함수에서 async와 같이 사용될 때, 데이터가 안 보이는 현상 발생
+const localStorageGet = (keys) => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(keys, (result) => resolve(result));
+  });
+};
 
 export { localStorageSet, localStorageGet };
