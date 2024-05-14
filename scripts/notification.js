656e38ee93aaa97a7e359e1225b2d13b8f814a4c
Squater0219 <141287114+Squater0219@users.noreply.github.com>
2024-05-14 23:15:51 +0900

    feat: 설정기능 연결

--- scripts/notification.js
+++ scripts/notification.js
@@ -1,29 +1,38 @@
 const createNotificationSignal = () => {
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.time) {
+    if (request.crawlingPeriod) {
       // 팝업 설정 페이지에서 설정한 크롤링 주기를 이용하여 알람을 생성합니다.
       chrome.alarms.create('crawlingPeriod', {
-        delayInMinutes: 1,
-        periodInMinutes: parseInt(request.time, 10),
+        delayInMinutes: 0,
+        periodInMinutes: parseInt(request.crawlingPeriod, 10),
       });
     }
   });
+  console.log("createnotificationsignal");
 };
 
 const createNotification = () => {
-  console.log("h")
-  // 나중에 크롤링한 데이터를 전후 비교하여 새 데이터만 출력되도록 짜야함
-  chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.notifications.create(
-      {
-        type: 'basic',
-        iconUrl: 'assets/img/iconImg.png',
-        title: 'testing',
-        message: 'doing testing..',
-        silent: false,
-      },
-      () => {},
-    );
+  let majorNoticeIsChange;
+  chrome.storage.onChanged.addListener((changes) => {
+    if (changes.majorNotices) {
+      majorNoticeIsChange = true;
+    }
+  });
+  chrome.alarms.onAlarm.addListener(async (alarm) => {
+    const { modalOnOff } = await chrome.storage.local.get('modalOnOff');
+    if (modalOnOff === true && majorNoticeIsChange === true) {
+      console.log("createnotification");
+      chrome.notifications.create(
+        {
+          type: 'basic',
+          iconUrl: 'assets/img/iconImg.png',
+          title: '새 공지가 등록되었습니다.',
+          message: 'doing testing..',
+          silent: false,
+        },
+        () => {},
+      );
+    }
   });
 };
 
